b630950d5c1604f4bd6ac07b69dabf68c54cc32f
Andrey Gud <nosacred@gmail.com>
2024-03-07 18:29:22 +0700

    try catch error checking

--- main.cpp
+++ main.cpp
@@ -21,22 +21,14 @@ static char *buffer[BUFFER_SIZE];
 
 bool end_of_file = false;
 
-void error_check(const char *reason, int err)
-{
-	if(err == -1) {
-		perror(reason);
-		exit(1);
-	}
-}
-
 void data_transfer(int srcfd, int destfd) {
 	int check = read(srcfd, buffer, BUFFER_SIZE);
-	error_check("Data recive fail", check);
+	if(-1 == check) throw "Data recive fail";
 	if(check == 0)
 		end_of_file = true;
 
 	check = write(destfd, buffer, check);
-	error_check("Data send fail", check);
+	if(-1 == check) throw "Data send fail";
 }
 
 int main(void)
@@ -48,56 +40,56 @@ int main(void)
 
 	std::cout << "Connecting to: " << SERV_ADDR << ':' << PORT << '\n';
 
-	sockfds[1] = socket(AF_INET, SOCK_STREAM, 0);
-	error_check("Socked failed", sockfds[1]);
+	try {
+		sockfds[1] = socket(AF_INET, SOCK_STREAM, 0);
+		if(-1 == sockfds[1]) throw "Socket failed";
 
-	servaddr.sin_family = AF_INET;
-	int check = inet_aton(SERV_ADDR, &servaddr.sin_addr);
-	if(check == 0) {
-		std::cerr << "IP convertion failed.\n";
-		return (1);
-	}
-	servaddr.sin_port = htons(PORT);
+		servaddr.sin_family = AF_INET;
+		int check = inet_aton(SERV_ADDR, &servaddr.sin_addr);
+		if(check == 0) {
+			std::cerr << "IP convertion failed.\n";
+			return (1);
+		}
+		servaddr.sin_port = htons(PORT);
 
-	check = connect(sockfds[1], 
-			reinterpret_cast<struct sockaddr*>(&servaddr), 
-			sizeof(servaddr));
-	error_check("Connection failed", check);
+		check = connect(sockfds[1], 
+				reinterpret_cast<struct sockaddr*>(&servaddr), 
+				sizeof(servaddr));
+		if(-1 == check) throw "Connection failed";
 
-	for(;;) {
-		fd_set readfds;
-		int max_d = sockfds[1];
+		for(;;) {
+			fd_set readfds;
+			int max_d = sockfds[1];
 
-		FD_ZERO(&readfds);
-		FD_SET(sockfds[0], &readfds);	// stdin
-		FD_SET(sockfds[1], &readfds);	// site
+			FD_ZERO(&readfds);
+			FD_SET(sockfds[0], &readfds);	// stdin
+			FD_SET(sockfds[1], &readfds);	// site
 
-		int res = select(max_d+1, &readfds, NULL, NULL, NULL);
-		if(res == -1) {
-			if(errno == EINTR) {
-				// recived signal error
-			} else {
-				// select error
+			int res = select(max_d+1, &readfds, NULL, NULL, NULL);
+			if(res == -1) throw "Recived signal error";
+
+			if(res == 0) {
+				// timeout
+				continue;
 			}
-			continue;
+
+			if(FD_ISSET(sockfds[0], &readfds)) 
+				data_transfer(sockfds[0], sockfds[1]);
+
+			if(FD_ISSET(sockfds[1], &readfds)) 
+				data_transfer(sockfds[1], sockfds[0]);
+
+			if(end_of_file)
+				break;
+
+			close(sockfds[1]);
 		}
 
-		if(res == 0) {
-			// timeout
-			continue;
-		}
-
-		if(FD_ISSET(sockfds[0], &readfds)) 
-			data_transfer(sockfds[0], sockfds[1]);
-
-		if(FD_ISSET(sockfds[1], &readfds)) 
-			data_transfer(sockfds[1], sockfds[0]);
-
-		if(end_of_file)
-			break;
+	} catch(const char *str) {
+		close(sockfds[1]);
+		perror(str);
+		return 1;
 	}
-
-	close(sockfds[1]);
 
 	return 0;
 }
